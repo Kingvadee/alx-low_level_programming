87db85802e907c675a6b0bd40b05b9c07f451df1
Kingvadee <ojeifod8@gmail.com>
2023-04-17 11:55:27 -0700

    Write a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints name of the file
+ *
+ * Return:Return Always (0)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
