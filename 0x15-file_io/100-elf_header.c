788d1dce1eb82f0cf44208171bcdbfeb0295e1f9
Kingvadee <ojeifod8@gmail.com>
2023-05-08 18:32:18 -0700

    Write a program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -42,3 +42,20 @@ int main(int argc, char *argv[])
 	printf("argc:%d, argv:%p\n", argc, (void *)*argv);
 	return (1);
 }
+
+void print_osabi(char *ptr)
+{
+char osabi = ptr[7];
+
+printf("  OS/ABI:                            ");
+if (osabi == 0)
+printf("UNIX - System V\n");
+else if (osabi == 2)
+printf("UNIX - NetBSD\n");
+else if (osabi == 6)
+printf("UNIX - Solaris\n");
+else
+printf("<unknown: %x>\n", osabi);
+
+printf("  ABI Version:                       %d\n", ptr[8]);
+}
